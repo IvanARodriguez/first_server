dc0376554b924c2c0de0fa2099efb9135da91402
Ivan Rodriguez <ivancalvo91@gmail.com>
2022-09-02 09:11:12 -0400

    creating first connection to TCP server with a 1024bit buffer

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 // Import the TCP rust built in library
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection established")
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("request: {}", String::from_utf8_lossy(&buffer[..]))
 }
